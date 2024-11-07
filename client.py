2770432e12c5b2b3aa89a0f33a9483ad7c86cfb2
pou-sou <shadow.of.laurent@gmail.com>
2024-11-07 22:09:41 +0330

    Created client.py

---
+++ client.py
@@ -0,0 +1,13 @@
+# client.py
+import socket
+
+client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client_socket.connect(('server_ip_here', 12345))  # Replace 'server_ip_here' with the actual IP of the server
+
+while True:
+    message = input("You: ")
+    client_socket.sendall(message.encode())  # Send message
+    data = client_socket.recv(1024)  # Receive reply
+    print("Server:", data.decode())
+
+client_socket.close()
