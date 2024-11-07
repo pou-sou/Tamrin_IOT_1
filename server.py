ad50dfa4f60af388d295a4513f8a56aa73e1835c
pou-sou <shadow.of.laurent@gmail.com>
2024-11-07 22:09:01 +0330

    Create server.py

---
+++ server.py
@@ -0,0 +1,20 @@
+# server.py
+import socket
+
+server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server_socket.bind(('0.0.0.0', 12345))  # 0.0.0.0 allows connections from any IP
+server_socket.listen(1)
+
+print("Server is waiting for a connection...")
+conn, addr = server_socket.accept()
+print(f"Connected by {addr}")
+
+while True:
+    data = conn.recv(1024)  # Receive message
+    if not data:
+        break
+    print("Client:", data.decode())
+    reply = input("You: ")
+    conn.sendall(reply.encode())  # Send reply
+
+conn.close()
