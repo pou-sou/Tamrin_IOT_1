4060565648fcbff1d5485dad713e91cf5192ce87
pou-sou <shadow.of.laurent@gmail.com>
2024-11-08 00:54:42 +0330

    dw

--- Source code/client.py
+++ Source code/client.py
@@ -2,10 +2,10 @@
 import socket
 
 client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-client_socket.connect(('server_ip_here', 12345))  # Replace 'server_ip_here' with the actual IP of the server
+client_socket.connect(('server_ip_here', 5050))  # Replace 'server_ip_here' with the actual IP of the server
 
 while True:
-    message = input("Mohammadamin0098: ")
+    message = input("You: ")
     client_socket.sendall(message.encode())  # Send message
     data = client_socket.recv(1024)  # Receive reply
     print("Server:", data.decode())
--- Source code/server.py
+++ Source code/server.py
@@ -2,7 +2,7 @@
 import socket
 
 server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-server_socket.bind(('0.0.0.0', 12345))  # 0.0.0.0 allows connections from any IP
+server_socket.bind(('0.0.0.0', 5050))  # 0.0.0.0 allows connections from any IP
 server_socket.listen(1)
 
 print("Server is waiting for a connection...")
